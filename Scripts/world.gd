1da8f43aeaf852480ea3cfee7553ae76d58df37d
Pranav <pranavbudlu@gmail.com>
2024-08-09 16:59:28 -0400

    added spawn boundaries

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -6,7 +6,7 @@ func _on_enemy_spawner_timeout():
 	var enemy_instance = enemy_scene.instantiate()  # Instantiate the enemy scene
 	if enemy_instance:
 		var enemy_position = Vector2(randi_range(-190, 190), randi_range(-105, 105))  # Random position within camera-view
-		while enemy_position.x < -25 and enemy_position.x > 25 or enemy_position.y < -30 or enemy_position.y > 30: #ensures enemy does not spawn on player
+		while enemy_position.x < -30 and enemy_position.x > 30 and enemy_position.y < -35 or enemy_position.y > 35: #ensures enemy does not spawn on player
 			enemy_position = Vector2(randi_range(-190, 190), randi_range(-105, 105))
 	
 		enemy_instance.position = enemy_position  # Set the enemy's positio

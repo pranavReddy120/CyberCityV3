bf58b0e87273e8ea902ac718d4ddf36ff0b249cb
tangerine238 <stephenwtang23@gmail.com>
2024-08-09 20:04:04 -0400

    player changes and added floor

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,9 +2,14 @@ extends CharacterBody2D
 
 
 const bullet_path = preload("res://Scenes/bullet.tscn")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
 
+const SPEED = 150
+const JUMP_VELOCITY = -300
 var speed = 300
 var can_shoot = true
+
 
 func shoot():
 	var bullet = bullet_path.instantiate() # instance of a bullet
@@ -20,3 +25,38 @@ func _process(delta):
 			can_shoot = false
 	elif Input.is_action_just_released('shoot'):
 		can_shoot = true
+		
+		
+# making movement
+# I was thinking arrows for movement and space for shoot. if we have interactions and maybe like inventory slots
+# We should use the number keys. we can have like movement in the right hand and interaction in the left
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	var direction = Input.get_axis("left","right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

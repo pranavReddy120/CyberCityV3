5be36195a9bbec382fc0adfcf1ea65acfd4c961f
tangerine238 <stephenwtang23@gmail.com>
2024-08-10 11:56:35 -0400

    fixing collision bug

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 
 func _on_area_2d_body_entered(body):
-	$AnimatedSprite2D.modulate = Color(1, 0, 0)  # RGB values for red
-	await get_tree().create_timer(0.2).timeout
-	queue_free() #removes enemy from scene
+	if body.is_in_group("bullet"):
+		$AnimatedSprite2D.modulate = Color(1, 0, 0)  # RGB values for red
+		await get_tree().create_timer(0.2).timeout
+		queue_free() #removes enemy from scene

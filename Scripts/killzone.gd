75ae8a1102988071f696e12b5cf1dff966002ee3
tangerine238 <stephenwtang23@gmail.com>
2024-08-16 14:58:49 -0400

    fixing broken stuff

---
+++ Scripts/killzone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer = $Timer
+var main_menu = preload("res://Scenes/main_menu.tscn")
+
+func _on_body_entered(body):
+	print("You died")
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+	
+	
+
+
+func _on_timer_timeout():
+	GameManager.transition_to_scene(main_menu.resource_path)

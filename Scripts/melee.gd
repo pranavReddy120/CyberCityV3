b7383537644485eb2e06b3f0854b1516d6fc3c8d
tangerine238 <stephenwtang23@gmail.com>
2024-08-19 20:57:51 -0400

    melee killing player

--- Scripts/melee.gd
+++ Scripts/melee.gd
@@ -35,20 +35,21 @@ func _on_timer_timeout():
 	GameManager.transition_to_scene(main_menu.resource_path)
 	
 func hit():
-	$Swing.monitering = true
-
-func end_hit():
-	$Swing.monitering = false
+	speed = 0
+	$AnimatedSprite2D.play("attack")
+	speed = 60
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
 	
 func start_walk():
 	$AnimatedSprite2D.play("run")
 
 func _on_attack_body_entered(body):
-	$AnimatedSprite2D.play("attack")
-	
-
-func _on_swing_body_entered(body):
-		if body.is_in_group("player"):
+	hit()
+	await get_tree().create_timer(0.5).timeout
+	if body.is_in_group("player"):
 			body.get_node("AnimatedSprite2D").modulate = Color(1,0,0)
 			await get_tree().create_timer(0.2).timeout
 			timer.start()
